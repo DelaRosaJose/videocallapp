c5f2c62db047bb7c6a2f02155b452bb94aaf0adc
Jose De la Rosa <josealberto3053@gmail.com>
2025-11-20 22:40:43 -0400

    Feat: Change to create the RoomID at the begining

--- lib/data/services/signaling_firebase_service.dart
+++ lib/data/services/signaling_firebase_service.dart
@@ -3,13 +3,36 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class SignalingFirebaseService {
   final _firestore = FirebaseFirestore.instance;
 
-  Future<String> createRoomAndSendOffer(Map<String, dynamic> offer) async {
+  Future<String> createRoomAndSendOffer(
+    String roomId,
+    Map<String, dynamic> offer,
+  ) async {
     try {
-      final DocumentReference roomRef = _firestore.collection('rooms').doc();
+      final DocumentReference roomRef = _firestore
+          .collection('rooms')
+          .doc(roomId);
 
       await roomRef.set({'sdp': offer});
 
       return roomRef.id;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> addIceCandidate({
+    required String roomId,
+    required Map<String, dynamic> candidate,
+    required bool isCaller,
+  }) async {
+    try {
+      final collectionName = isCaller ? 'callerCandidates' : 'guestCandidates';
+
+      await _firestore
+          .collection('rooms')
+          .doc(roomId)
+          .collection(collectionName)
+          .add(candidate);
     } catch (e) {
       rethrow;
     }
--- lib/features/call/cubit/call_cubit.dart
+++ lib/features/call/cubit/call_cubit.dart
@@ -1,26 +1,26 @@
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:videocallapp/data/services/signaling_firebase_service.dart';
 import 'package:videocallapp/features/call/cubit/call_state.dart';
 
 class CallCubit extends Cubit<CallState> {
-  CallCubit(this._signalingRepository) : super(CallInitial());
+  CallCubit(this._signalingService) : super(CallInitial());
 
-  final SignalingFirebaseService _signalingRepository;
+  final SignalingFirebaseService _signalingService;
+  String? _roomID = null;
 
+  MediaStream? _localStream;
+  RTCVideoRenderer? _localRenderer;
   RTCPeerConnection? _peerConnection;
 
   Future<void> createCall() async {
     emit(CallConnecting());
     try {
-      final configuration = {
-        'iceServers': [
-          {'urls': 'stun:stun.l.google.com:19302'},
-        ],
-      };
-
-      //Creamos nuestra conexion local
-      _peerConnection = await createPeerConnection(configuration);
+      _roomID = generateRoomId();
+      await _initializeAndConnect();
 
       //Creamos la oferta SDP
       final offer = await _peerConnection!.createOffer();
@@ -28,14 +28,62 @@ class CallCubit extends Cubit<CallState> {
       //Configuramos la oferta localmente
       await _peerConnection!.setLocalDescription(offer);
 
-      //Enviamos la oferta al servidor y obtenemos el ID de la sala
-      final roomId = await _signalingRepository.createRoomAndSendOffer(
-        offer.toMap(),
-      );
+      //Enviamos la oferta al servidor
+      await _signalingService.createRoomAndSendOffer(_roomID!, offer.toMap());
 
       emit(CallInitial());
     } catch (e) {
       emit(CallFailure(e.toString()));
     }
+  }
+
+  Future<void> _initializeAndConnect() async {
+    final configuration = {
+      'iceServers': [
+        {'urls': 'stun:stun.l.google.com:19302'},
+      ],
+    };
+
+    _peerConnection = await createPeerConnection(configuration);
+
+    _setupListeners();
+
+    // 3. Obtener media del usuario.
+    _localStream = await navigator.mediaDevices.getUserMedia({
+      'audio': true,
+      'video': {'facingMode': 'user'},
+    });
+    // ... conectar stream al renderer, etc.
+  }
+
+  void _setupListeners() {
+    // No quitar by: JRuiz, En Web platform hay un bug en la libreria que no permite recibir los ICE candidates, encontramos esta solución en los Issues de flutter-webrtc
+    if (kIsWeb) {
+      _peerConnection?.addTransceiver(
+        kind: RTCRtpMediaType.RTCRtpMediaTypeVideo,
+        init: RTCRtpTransceiverInit(direction: TransceiverDirection.RecvOnly),
+      );
+    }
+
+    // Escuchamos los ICE
+    _peerConnection!.onIceCandidate = (candidate) {
+      _signalingService.addIceCandidate(
+        roomId: _roomID!,
+        candidate: candidate.toMap(),
+        isCaller: true, // ¡Importante! Debemos saber quién envía el candidato.
+      );
+    };
+
+    // SETEAMOS EL LISTENER DE PISTAS DE VIDEO/AUDIO
+    _peerConnection!.onTrack = (event) {
+      print("🛰️ ¡Pista remota recibida!");
+      // Lógica para mostrar el video del otro usuario.
+    };
+  }
+
+  String generateRoomId() {
+    var r = Random();
+    int randomInt = r.nextInt(900000) + 100000;
+    return randomInt.toString();
   }
 }
